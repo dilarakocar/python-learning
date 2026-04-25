8db4324a83bc50fe5eab819ff28167989aa0a8fe
Ayşe Dilara Koçar <dilarakocar@gmail.com>
2026-04-26 00:46:19 +0300

    Update 1.if-else.py

--- week2/1.if-else.py
+++ week2/1.if-else.py
@@ -1,4 +1,6 @@
-if su_seviyesi>60:
-  print("Su tahliye et")
-else: 
-  print("Suyu doldurmaya devam et")
+water_level = 70
+
+if water_level > 60:
+    print("Drain the water")
+else:
+    print("Continue filling the water")
