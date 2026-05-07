8cca9f98224079ad8dd887db7f4e086203e39b8c
Ayşe Dilara Koçar <dilarakocar@gmail.com>
2026-05-07 19:45:27 +0300

    Create 5.random-module.py

---
+++ week3/5.random-module.py
@@ -0,0 +1,10 @@
+import random
+import my_module
+
+# random.randint(20, 30)
+# Returns a random number between 20 and 30 (inclusive)
+
+random_number = random.randint(1, 10)
+
+# Prints a different number every time the program runs
+print(random_number)
